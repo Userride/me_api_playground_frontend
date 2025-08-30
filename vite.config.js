437b668433171dfdcd64fea7d4dd8e47c1f2c681
prince kumar <singhprince131074@gmail.com>
2025-08-30 15:53:49 +0530

    Fix Vercel deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
+  // 👇 Needed for Vercel or other static hosts
+  build: {
+    outDir: 'dist',
+  },
 })
